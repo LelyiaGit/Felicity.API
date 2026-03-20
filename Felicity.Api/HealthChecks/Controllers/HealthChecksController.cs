62ab102dbd5fdb7e2821345643f1471cd2dcc395
LelyiaGit <lelyia@outlook.com>
2026-03-20 22:34:25 +0100

    HealthChecks controller added

---
+++ Felicity.Api/HealthChecks/Controllers/HealthChecksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Felicity.Api.HealthChecks.Controllers;
+
+
+[ApiController]
+[Route("health")]
+public class HealthChecksController : Controller
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("pong");
+    }
+}
